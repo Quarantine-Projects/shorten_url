9601d1fc70da6e47ffd56d8e271170046fb7c051
salil-gtm <salilgautam_15037@aitpune.edu.in>
2020-03-30 02:37:36 +0530

    shorten_url.py : function name fix

--- shorten_url/shorten_url.py
+++ shorten_url/shorten_url.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def url_short(url):
+def short(url):
     response = requests.post('https://rel.ink/api/links/', data={"url": url})
     if response.status_code == 201:
         data = json.dumps(response.json())
@@ -11,7 +11,7 @@ def url_short(url):
         return response.status_code
     return output
 
-def url_expand(url):
+def expand(url):
     response = requests.get('https://rel.ink/api/links/' + url.split('rel.ink/')[1])
     if response.status_code == 200:
         data = json.dumps(response.json())
